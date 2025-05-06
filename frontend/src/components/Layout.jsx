294417d8bfe8e2dc49ba7a63f49284cab082bc55
kanchanp <kanchanp937@gmail.com>
2025-05-06 16:30:06 +0530

    made dashboard responsive

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,20 +1,20 @@
-// layout.jsx
 import React from 'react';
 
 const Layout = ({ children }) => {
   return (
     <div
       style={{
-        width: '1600px',
-        height: '6255px',
-        margin: '0 auto', // center the layout horizontally
-        // padding: '20px', 
+        width: '100%',
+        maxWidth: '1601px', // keeps your design's intended width
+        minHeight: '100vh', // full height but allows content to grow
+        maxHeight:'6255px',
+        margin: '0 auto',
+        // padding: '1rem',
         boxSizing: 'border-box',
-        backgroundColor: '#FFFFFF', 
+        backgroundColor: '#FFFFFF',
         display: 'flex',
-         flexDirection: 'column', 
-         alignItems: 'center', 
-        // justifyContent:'center',
+        flexDirection: 'column',
+        alignItems: 'center',
       }}
     >
       {children}
