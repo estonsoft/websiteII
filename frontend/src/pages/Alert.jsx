a5870bdf26f4a05ed802c1fe15b9b4d910793f03
kanchanp <kanchanp937@gmail.com>
2025-05-06 18:19:09 +0530

    responsive alert component

--- frontend/src/pages/Alert.jsx
+++ frontend/src/pages/Alert.jsx
@@ -4,19 +4,26 @@ import './Alert.css';
 const Alert = () => {
   return (
     <div className="alert-container">
-      <div className="alert-item alert-title">10k+</div>
-      <div className="alert-item alert-subtitle">Customers trusted EstonSoft</div>
+        <div className=" alert-item alert-group1 ">
+          <div className=" alert-title">10k+</div>
+          <div className=" alert-description">Customers trusted EstonSoft</div>
+        </div>
       
+
+
       <div className="alert-item alert-percentage-container">
-        <div className="alert-percentage">93%</div>
+        <div className="alert-title">93%</div>
         <div className="alert-description">Satisfaction rate from our customers.</div>
       </div>
-      
+
+
+
       <div className="alert-item alert-rating-container">
-        <div className="alert-percentage">4.9</div>
+        <div className="alert-title">4.9</div>
         <div className="alert-description">Average customer ratings out of 5.00!</div>
       </div>
     </div>
+
   );
 };
 
